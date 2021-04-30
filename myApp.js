87762667ec9825829bd1f3601a791c5022c16e4b
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-04-30 18:18:39 +0300

    refactored to arrow function

--- myApp.js
+++ myApp.js
@@ -51,7 +51,7 @@ app.get("/now", (req, res, next) => {
     res.json({time: req.time});
 });
 
-app.get("/:word/echo/", function (req, res){
+app.get("/:word/echo/", (req, res) => {
   res.json({ echo: req.params.word });
 });
 
