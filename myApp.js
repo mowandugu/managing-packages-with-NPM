fa639ff04a3d7a35bdd42f4268f48a1a7958ad51
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-04-30 12:46:01 +0300

    change to arrow functions

--- myApp.js
+++ myApp.js
@@ -12,12 +12,12 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
   });
 
 
-app.get("/json", function(req, res){
+app.get("/json", (req, res) => {
     res.json({"message": "Hello json"});
 });
 
