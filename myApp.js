a132b8bc8e2861b47f2c1bbd9c55030580d98ff9
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-05-04 11:19:35 +0300

    add post api endpoint to myApp

--- myApp.js
+++ myApp.js
@@ -67,7 +67,9 @@ app.get("/name", (req, res) => {
     res.json({ name: req.query.first + " " + req.query.last})
 })
 
-
+app.post("/name", (req, res) => {
+    res.json({ name: req.body.first + " " + req.body.last})
+})
 
 
 
